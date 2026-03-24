48c5197aada7c1a6fb901761b9f85a27bce10f32
TomChienXu <truongss04@gmail.com>
2026-03-24 13:57:39 +0700

    update

--- src/pages/Teacher.tsx
+++ src/pages/Teacher.tsx
@@ -1,4 +1,4 @@
-import { Button, message, Popconfirm, Table, Tabs } from "antd";
+import { Button, Form, Input, message, Modal, Popconfirm, Table, Tabs } from "antd";
 import MainLayout from "../layouts/MainLayout";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -10,7 +10,9 @@ const Teacher = () => {
     const [limit, setLimit] = useState<number>(10);
     const [total, setTotal] = useState<number>(1);
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
+    const [form] = Form.useForm();
 
 
 
@@ -23,7 +25,7 @@ const Teacher = () => {
                     Authorization: `Bearer ${localStorage.getItem('token')}`
                 }
             });
-            console.log(response, ' res');
+            // console.log(response, ' res');
             setTotal(response.data.total);
             setData(response.data.data);
         } catch (error) {
@@ -32,9 +34,9 @@ const Teacher = () => {
         }
         setLoading(false);
     };
-    console.log(data, 'data')
+    // console.log(data, 'data')
 
-    const handleDeleteStudent = async (teacaherId: string) => {
+    const handleDeleteTeacher = async (teacaherId: string) => {
         try {
             await axios.delete(`http://103.166.183.82:4040/api/v1/teacher/${teacaherId}`, {
                 headers: {
@@ -54,6 +56,26 @@ const Teacher = () => {
             message.error("Có lỗi xảy ra khi xóa sinh viên!");
         }
     };
+
+    const handleAddTeacher = async (values: any) => {
+        setLoading(true);
+        try {
+            await axios.post("http://103.166.183.82:4040/api/v1/teacher", values,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                }
+            );
+            message.success("Thêm giảng viên thành công!");
+            setIsModalOpen(false);
+            form.resetFields();
+            fetchData();
+        } catch (error) {
+            console.error("Error adding student:", error);
+            message.error("Có lỗi xảy ra khi thêm giảng viên!");
+        }
+    }
 
 
     interface Teacher {
@@ -82,7 +104,7 @@ const Teacher = () => {
             key: "department",
         },
         {
-            title: "Mã sinh viên",
+            title: "Mã giảng viên",
             dataIndex: "identityCode",
             key: "identityCode",
         },
@@ -92,14 +114,14 @@ const Teacher = () => {
             key: "phoneNumber",
         },
         {
-            title: "Hành động",
+            title: "",
             key: "action",
             render: (_: any, record: Teacher) => (
                 <Popconfirm
                     title="Xóa sinh viên"
                     description={`Bạn có chắc chắn muốn xóa sinh viên ${record.fullName} không?`}
                     // 3. Trỏ đúng vào record._id khi click xóa
-                    onConfirm={() => handleDeleteStudent(record._id)}
+                    onConfirm={() => handleDeleteTeacher(record._id)}
                     okText="Đồng ý"
                     cancelText="Hủy"
                 >
@@ -119,10 +141,40 @@ const Teacher = () => {
                 items={[{
                     key: '1',
                     label: <h2>Quản lý giảng viên</h2>,
-                    children: <Button type="primary">Thêm mới</Button>,
+                    children: <Button type="primary" onClick={() => { setIsModalOpen(true) }}>Thêm mới</Button>,
                 }]}
                 style={{ marginBottom: 20 }}
             />
+            <Modal
+                title="Basic Modal"
+                closable={{ 'aria-label': 'Custom Close Button' }}
+                open={isModalOpen}
+                onOk={() => form.submit()}
+                onCancel={() => { setIsModalOpen(false) }}
+            >
+                <Form
+                    form={form}
+                    layout="vertical"
+                    onFinish={handleAddTeacher}
+                >
+
+                    <Form.Item name="fullName" label="Họ tên" rules={[{ required: true, message: 'Vui lòng nhập họ tên!' }]}>
+                        <Input placeholder="Nhập họ và tên" />
+                    </Form.Item>
+                    <Form.Item name="email" label="Email" rules={[{ required: true, message: 'Vui lòng nhập email!' }, { type: 'email', message: 'Email không đúng định dạng!' }]}>
+                        <Input placeholder="Nhập địa chỉ email" />
+                    </Form.Item>
+                    <Form.Item name="department" label="Lớp hành chính" rules={[{ required: true, message: 'Vui lòng nhập lớp hành chính!' }]}>
+                        <Input placeholder="Nhập lớp hành chính" />
+                    </Form.Item>
+                    <Form.Item name="identityCode" label="Mã sinh viên" rules={[{ required: true, message: 'Vui lòng nhập mã sinh viên!' }]}>
+                        <Input placeholder="Nhập mã giảng viên" />
+                    </Form.Item>
+                    <Form.Item name="phoneNumber" label="Số điện thoại" rules={[{ required: true, message: 'Vui lòng nhập số điện thoại!' }]}>
+                        <Input placeholder="Nhập số điện thoại" />
+                    </Form.Item>
+                </Form>
+            </Modal>
             <Table
                 columns={columns}
                 dataSource={data}
